c4dc77fd9a77f1b19d93ba5f4b3866d3938eea4d
rimskyrocha <rimskyrocha@gmail.com>
2026-03-29 20:20:40 -0300

    Create PatientEvolutionRepository.java

---
+++ consultappbdmigrawind/consult app/backend_migration/src/main/java/com/odontologia/repository/PatientEvolutionRepository.java
@@ -0,0 +1,12 @@
+package com.odontologia.repository;
+
+import com.odontologia.model.PatientEvolution;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PatientEvolutionRepository extends JpaRepository<PatientEvolution, String> {
+    List<PatientEvolution> findByPatientId(String patientId);
+}
