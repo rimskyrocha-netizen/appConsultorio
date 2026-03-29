b8d2aad20dea572112da3d38881d2c38dc32075f
rimskyrocha <rimskyrocha@gmail.com>
2026-03-29 20:21:01 -0300

    Create UserAccountRepository.java

---
+++ consultappbdmigrawind/consult app/backend_migration/src/main/java/com/odontologia/repository/UserAccountRepository.java
@@ -0,0 +1,11 @@
+package com.odontologia.repository;
+
+import com.odontologia.model.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+    Optional<UserAccount> findByEmail(String email);
+}
