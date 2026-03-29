494dfbd515ccba49fe30dba2a1e6a7708dabdddb
rimskyrocha <rimskyrocha@gmail.com>
2026-03-29 20:17:09 -0300

    Create PatientRepository.java

---
+++ consultappbdmigrawind/consult app/backend/src/main/java/com/odontologia/repository/PatientRepository.java
@@ -0,0 +1,9 @@
+package com.odontologia.repository;
+
+import com.odontologia.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, String> {
+}
